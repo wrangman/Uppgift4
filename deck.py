539ca000fc6c64a2155f815e78fe48ba97513f8c
wrangman <wrangman@gmail.com>
2024-09-11 11:13:37 +0200

    dealfunc

--- deck.py
+++ deck.py
@@ -8,32 +8,43 @@ class Card:
     def __str__(self):
         return f"{self.suit} {self.value}"
     
+    def __repr__(self):
+        return f"{self.suit} {self.value}"
+        
     
 class Deck:
-    def __init__(self, cards=None):
+    def __init__(self, cards=None): 
         if cards is None:
             cards = []
         self.cards = cards
         
     def show_all(self):
-        for card in self.cards:
-            if card.suit == "♥" or card.suit == "♦": 
+        for card in self.cards: 
+            if card.suit == "♥" or card.suit == "♦": #om det är röda kort ändra färgen på texten med denna ansi-kod
                 print(f"\033[31m{card}\033[0m")
             else:
                 print(f"{card}")
 
-    @staticmethod
+    def deal(self, num_cards):
+        if num_cards > len(self.cards):
+            raise ValueError("Not enough cards left to deal")
+        
+        dealt_cards = self.cards[:num_cards]
+        self.cards = self.cards[num_cards:]  # Remove dealt cards from the deck
+        
+        return dealt_cards
+    
+    @staticmethod           # en metod som fungerar likt vanlig funktion. en @staticmethod tar inte emot instansen (self) 
     def make_deck():  
         cards = [] 
         suits = ["♠", "♥", "♣", "♦"]
         values = ["Ess", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
         
-        for suit in range(4):   #suits
-            current_suit = suits[suit] 
+        for suit in suits:   # 4 suits
+            current_suit = suit 
 
-            for value in range(13): #values
-                current_value = values[value]
-                # print(current_value, current_suit)
+            for value in values: # 13 values per suit
+                current_value = value
                 cards.append(Card(current_suit, current_value))
         
         return cards
@@ -44,6 +55,9 @@ os.system('cls' if os.name == 'nt' else 'clear')
 cards = Deck.make_deck()
 
 deck = Deck(cards)
+
+# dealt_card = deck.deal(51)
+# print(dealt_card[0])
+
+
 deck.show_all()
-
-
