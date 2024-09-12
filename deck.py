9c9852a395cd5f29eca3e6663ea9f2b55c6f8582
wrangman <wrangman@gmail.com>
2024-09-12 14:36:36 +0200

    sad

--- deck.py
+++ deck.py
@@ -14,14 +14,11 @@ class Card:
     
 class Deck:
     def __init__(self, cards=None): 
-    def __init__(self, cards=None): 
         if cards is None:
             cards = []
         self.cards = cards
         
     def show_all(self):
-        for card in self.cards: 
-            if card.suit == "♥" or card.suit == "♦": #om det är röda kort ändra färgen på texten med denna ansi-kod
         for card in self.cards: 
             if card.suit == "♥" or card.suit == "♦": #om det är röda kort ändra färgen på texten med denna ansi-kod
                 print(f"\033[31m{card}\033[0m")
@@ -43,17 +40,13 @@ class Deck:
         suits = ["♠", "♥", "♣", "♦"]
         values = ["Ess", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
         
-        for suit in suits:   # 4 suits
-            current_suit = suit 
+       
         for suit in suits:   # 4 suits
             current_suit = suit 
 
             for value in values: # 13 values per suit
                 current_value = value
-            for value in values: # 13 values per suit
-                current_value = value
-                cards.append(Card(current_suit, current_value))
-        
+                cards.append(Card(current_suit, current_value)) 
         return cards
 
 
